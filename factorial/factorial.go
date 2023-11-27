e1253210979bde9c2cdc52debead4b93baa56d26
Zakhar Podporinov <zp162162@gmail.com>
2023-11-27 19:27:52 +0200

    Improved readability in factorial.go

--- factorial/factorial.go
+++ factorial/factorial.go
@@ -7,29 +7,32 @@ import (
 
 func factorial(n int) *big.Int {
 	fact := big.NewInt(1)
+
 	for i := 2; i <= n; i++ {
 		fact.Mul(fact, big.NewInt(int64(i)))
 	}
+
 	return fact
 }
 
 func CalculateFactorials(a, b int) map[string]*big.Int {
-	var wg sync.WaitGroup
+	var waitGroup sync.WaitGroup
 	result := make(map[string]*big.Int)
 
 	var a_factorial, b_factorial *big.Int
 
-	wg.Add(2)
+	waitGroup.Add(2)
 	go func() {
-		defer wg.Done()
+		defer waitGroup.Done()
 		a_factorial = factorial(a)
 	}()
+
 	go func() {
-		defer wg.Done()
+		defer waitGroup.Done()
 		b_factorial = factorial(b)
 	}()
 
-	wg.Wait()
+	waitGroup.Wait()
 
 	result["a_factorial"] = a_factorial
 	result["b_factorial"] = b_factorial
